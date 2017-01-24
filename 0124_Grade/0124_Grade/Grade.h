b7d83f7286c761c8f9731680c2052d6839d790f2
i.minjung.chun <indie.me.chun@gmail.com>
2017-01-24 21:59:13 +0900

    0124. Grade 점수매기기

---
+++ 0124_Grade/0124_Grade/Grade.h
@@ -0,0 +1,13 @@
+//
+//  Grade.h
+//  0124_Grade
+//
+//  Created by katniss on 2017. 1. 24..
+//  Copyright © 2017년 katniss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Grade : NSObject
+
+@end
---
+++ 0124_Grade/0124_Grade/Grade.m
@@ -0,0 +1,13 @@
+//
+//  Grade.m
+//  0124_Grade
+//
+//  Created by katniss on 2017. 1. 24..
+//  Copyright © 2017년 katniss. All rights reserved.
+//
+
+#import "Grade.h"
+
+@implementation Grade
+
+@end
