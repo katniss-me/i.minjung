a1f2ec642c9af83cc31da526ed71e6021eba441a
i.minjung.chun <indie.me.chun@gmail.com>
2017-02-01 17:34:59 +0900

    0201 while과 for문을 이용한 절대값 구하기, 구구단 구현, 팩토리얼 문제

---
+++ 0201_Carculator2/0201_Carculator2/Carculator.h
@@ -0,0 +1,13 @@
+//
+//  Carculator.h
+//  0201_Carculator2
+//
+//  Created by katniss on 2017. 2. 1..
+//  Copyright © 2017년 katniss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Carculator : NSObject
+
+@end
